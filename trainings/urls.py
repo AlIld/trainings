75a2eab46473adbd9a059ed1ca44b4a40ea9f950
AlIld <gubaewa.alsu@yandex.ru>
2020-04-19 18:43:26 +0300

    Add password change and reset

--- trainings/urls.py
+++ trainings/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path
 from user.views import registration_view, logout_view, login_view, account_view
 from personal.views import home_screen_view
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,4 +26,23 @@ urlpatterns = [
     path('login/', login_view, name = "login"),
     path('account/', account_view, name = "account"),
     path('', home_screen_view, name = "home"),
+
+    # Password reset links (ref: https://github.com/django/django/blob/master/django/contrib/auth/views.py)
+    path('password_change/done/',
+         auth_views.PasswordChangeDoneView.as_view(template_name='registration/password_change_done.html'),
+         name='password_change_done'),
+
+    path('password_change/', auth_views.PasswordChangeView.as_view(template_name='registration/password_change.html'),
+         name='password_change'),
+
+    path('password_reset/done/',
+         auth_views.PasswordResetCompleteView.as_view(template_name='registration/password_reset_done.html'),
+         name='password_reset_done'),
+
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+
+    path('reset/done/',
+         auth_views.PasswordResetCompleteView.as_view(template_name='registration/password_reset_complete.html'),
+         name='password_reset_complete'),
 ]
